e4783281a0068bb08d1bbb932c2e327a36ec4011
danknights <danknights@gmail.com>
2014-07-28 12:36:00 -0500

    added comments

--- bin/get_kmers_from_fasta.py
+++ bin/get_kmers_from_fasta.py
@@ -34,38 +34,62 @@ def make_option_parser():
 
 
 if __name__ == '__main__':
+	# make option parser and parse command line flags
     parser = make_option_parser()
     (options, args) = parser.parse_args()
     
     k = options.kmer_size
     input_fp = options.input_fasta
+    
+    # create and open output file if needed
     if options.output_file is not None:
         output_file = open(options.output_file,'w')
 
-    kmers = set() # dict to hold unique kmers
+
+    kmers = set() # set to hold unique kmers, faster than dict or ordered list
 
     # print new kmers as we read them
     # this will reduce memory requirements because
     # we don't have to store the sequence IDs
     count = 0
     for line in open(input_fp,'U'):
+
         if line.startswith('>'):
+        	# if this is a sequence header
+
             count += 1
             if options.verbose and count % 100000 == 0:
                 print count
+                
+            # extract only the sequence ID (split on whitespace, first element)
             seq_id = line[1:].split()[0]
         else:
+        	# if this line contains sequence
+
+        	# remove trailing whitespace characters
             seq = line.strip()
+            
             if len(seq) >= k:
+	            # if this sequence is long enough to have a k-mer
+
+				# step through sequence on base at a time
                 for i in xrange(0,len(seq)-k + 1):
+                	# current kmer is a substring of length k
                     kmer = seq[i:i+k]
+                    
                     if not kmer in kmers:
+                    	# this kmer is not in the current set of kmers; add it
                         kmers.add(kmer)
+                        
+                    	# print the kmer (and its sequence ID if fasta output)
                         if options.output_type == 'text' or options.output_type == 'fasta':
                             if options.output_type == 'fasta':
-                                output_file.write('>%s_%09d' %(seq_id,i) + '\n')
+                            	kmer_id = '>%s_%09d' %(seq_id,i)
+                                output_file.write(kmer_id + '\n')
                             output_file.write(kmer + '\n')
+
     if options.output_type == 'count':
+	    # print count if requested
         if options.output_file is not None:
             output_file.write(str(len(kmers)) + '\n')
         else:
