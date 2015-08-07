23046ac097155aa2e1cbe0c37df88fa344208151
Dan Knights <dknights@umn.edu>
2015-08-07 10:18:15 -0500

    Fixed tabs

--- bin/find_distinct_taxa.py
+++ bin/find_distinct_taxa.py
@@ -35,47 +35,47 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
 
 
-	# build taxonomy map
-	print "Loading taxonomy map..."
-	taxa = {}
-	for line in open(options.taxonomy_file,'r'):
-	    words = line.strip().split('\t')
-	    taxon_ID = words[0]
-	    taxonomy = words[1].split(';')
-	    taxonomy = [level.strip() for level in taxonomy]
-	    taxa[taxon_ID] = taxonomy
-	
-	# Read through otu_by_otu matches
-	# For each otu, if it's in the taxonomy dict
-	# add it to the list of those with species-level taxonomy
-	#
-	# if it matches any other otus in a different species,
-	# add it to the ambiguous list
-	potential_list = set()
-	bad_list = set()
+    # build taxonomy map
+    print "Loading taxonomy map..."
+    taxa = {}
+    for line in open(options.taxonomy_file,'r'):
+        words = line.strip().split('\t')
+        taxon_ID = words[0]
+        taxonomy = words[1].split(';')
+        taxonomy = [level.strip() for level in taxonomy]
+        taxa[taxon_ID] = taxonomy
+    
+    # Read through otu_by_otu matches
+    # For each otu, if it's in the taxonomy dict
+    # add it to the list of those with species-level taxonomy
+    #
+    # if it matches any other otus in a different species,
+    # add it to the ambiguous list
+    potential_list = set()
+    bad_list = set()
 
-	for line in open(options.input_fp,'U'):
-		words = line.split('\t')
-		query = words[0].split()[0]
-		ref = words[1].split()[0]
-		
-		# only keep this as potential if it resolves to species
-		if len(taxa[query]) > 7:
-			if taxa[query][6] != 's__' and taxa[query[6]] != 'unidentified':
-				potential_list.add(query)
-		
-			# add to bad list if its match has a different species
-			if taxa[ref][6] != taxa[query][6]:
-				bad_list.add(query)
+    for line in open(options.input_fp,'U'):
+        words = line.split('\t')
+        query = words[0].split()[0]
+        ref = words[1].split()[0]
+        
+        # only keep this as potential if it resolves to species
+        if len(taxa[query]) > 7:
+            if taxa[query][6] != 's__' and taxa[query[6]] != 'unidentified':
+                potential_list.add(query)
+        
+            # add to bad list if its match has a different species
+            if taxa[ref][6] != taxa[query][6]:
+                bad_list.add(query)
 
-	# print only those in the good list and not the bad list
-	good_taxa = set()
-	for key in potential_list:
-		if not key in bad_list:
-			good_taxa.add('; '.join(taxa[key]))
+    # print only those in the good list and not the bad list
+    good_taxa = set()
+    for key in potential_list:
+        if not key in bad_list:
+            good_taxa.add('; '.join(taxa[key]))
 
-	outf = open(options.output_fp,'w')
-	for taxon in sorted(good_taxa):
-		outf.write(taxon + '\n')
-	outf.close()
-	
+    outf = open(options.output_fp,'w')
+    for taxon in sorted(good_taxa):
+        outf.write(taxon + '\n')
+    outf.close()
+    
