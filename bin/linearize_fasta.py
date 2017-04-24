105a9640da47fc6c058378f053e75ab2655be5c9
Dan Knights <danknights@x-10-104-228-220.uofm-secure.wireless.umn.edu>
2017-04-24 12:04:42 -0500

    py3

--- bin/linearize_fasta.py
+++ bin/linearize_fasta.py
@@ -3,17 +3,18 @@
 # usage:
 # linearize_fasta.py < input.fasta > output.fasta
 
+from __future__ import print_function
 import sys
 
 seq = ''
 for line in sys.stdin.readlines():
     if line.startswith('>'):
         if len(seq) > 0:
-            print seq
-        print line.strip()
+            print(seq)
+        print(line.strip())
         seq = ''
     else:
         seq += line.strip()
 
 # print last seq
-print seq
+print(seq)
